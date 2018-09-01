698a0363e09ced1ce2d06ef8f163359b669c859d
roderick <778683464@qq.com>
2018-09-01 15:01:20 +0800

    modify template

--- public/index.php
+++ public/index.php
@@ -8,14 +8,13 @@
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
-
 // [ 应用入口文件 ]
 namespace think;
-
 // 加载基础文件
 require __DIR__ . '/../thinkphp/base.php';
-
+define('APP_PATH', __DIR__ . '/../application/');
+define('DS', DIRECTORY_SEPARATOR);
+defined('ROOT_PATH') or define('ROOT_PATH', dirname(realpath(APP_PATH)) . DS);
 // 支持事先使用静态方法设置Request对象和Config对象
-
 // 执行应用并响应
 Container::get('app')->run()->send();
